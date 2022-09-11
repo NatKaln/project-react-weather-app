c420890c3f3f03948bb1463fb1fc18b6cb281724
Nataly <vslshnat@gmail.com>
2022-09-11 20:45:44 +0300

    trying to add css to component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Weather from "./Weather";
-import "./Weather.css";
+
 function App() {
   return (
     <div className="App">
