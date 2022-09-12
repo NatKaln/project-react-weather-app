8a63b6bab93c343b61c13b327ea3b131513be766
Nataly <vslshnat@gmail.com>
2022-09-12 18:16:28 +0300

    added link about me

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <p>
+        This project was coded by Nat Kalnytska and is{" "}
+        <a
+          href="https://github.com/NatKaln/project-react-weather-app/tree/master/src"
+          target="_balnk"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub
+      </p>
     </div>
   );
 }
