a322de4b45971c66204004dbcadf7c7033e5d720
Nataly <vslshnat@gmail.com>
2022-09-17 12:57:23 +0300

    changed stylying for icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -26,7 +26,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="fff"
-      size={64}
+      size={60}
       animate={true}
     />
   );
