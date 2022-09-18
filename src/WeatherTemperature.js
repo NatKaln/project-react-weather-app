52586812c61de8f648ce24036cc7466d3f354960
Nataly <vslshnat@gmail.com>
2022-09-18 11:48:13 +0300

    made fahrenheit function for calculating

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -11,6 +11,9 @@ export default function WeatherTemperature(props) {
     setUnit("celsius");
   }
 
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
   if (unit === "celsius") {
     return (
       <div>
@@ -25,10 +28,9 @@ export default function WeatherTemperature(props) {
       </div>
     );
   } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div>
-        <span className="temperature">{Math.round(fahrenheit)}</span>
+        <span className="temperature">{Math.round(fahrenheit())}</span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
             °C /{" "}
